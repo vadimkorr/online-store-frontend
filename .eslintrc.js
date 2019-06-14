9b5878b0eb98ec5ac2ecbbfd83d8a1f6a0e416e2
Vadim <korobeinikovv@mail.ru>
2019-06-14 19:25:36 +0300

    Applied linter

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb'],
+  env: {
+    browser: true,
+    node: true,
+  },
+  rules: {
+    'no-underscore-dangle': 0,
+    'linebreak-style': 0,
+    'import/no-unresolved': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+  },
+};
