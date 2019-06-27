85c801f29d383a6b4afe8fabebb1c53779c12bd4
Vadim <korobeinikovv@mail.ru>
2019-06-27 17:35:56 +0300

    Added no items handler to orders

--- src/screens/admin/OrdersScreen/OrdersScreen.tsx
+++ src/screens/admin/OrdersScreen/OrdersScreen.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { OrdersTable } from './OrdersTable';
+import { NoItemsDetect } from '../../../shared';
+import { AppState } from '../../../store';
 
-export const OrdersScreen = (): JSX.Element => (
-  <React.Fragment>
-    <OrdersTable />
-  </React.Fragment>
-);
+interface StateProps {
+  isNoItems: boolean;
+}
+type Props = StateProps;
+
+export const OrdersScreenInner = (props: Props): JSX.Element => {
+  const { isNoItems } = props;
+  return (
+    <NoItemsDetect isNoItems={isNoItems}>
+      <OrdersTable />
+    </NoItemsDetect>
+  );
+};
+
+// TODO: add reselect
+const mapStateToProps = (state: AppState) => {
+  const { items } = state.orders.admin;
+  return { isNoItems: items.length < 1 };
+};
+
+export const OrdersScreen = connect(mapStateToProps)(OrdersScreenInner);
--- src/screens/customer/OrdersScreen/OrdersScreen.tsx
+++ src/screens/customer/OrdersScreen/OrdersScreen.tsx
@@ -1,8 +1,27 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import { OrdersTable } from './OrdersTable';
+import { AppState } from '../../../store';
+import { NoItemsDetect } from '../../../shared';
 
-export const OrdersScreen = (): JSX.Element => (
-  <Fragment>
-    <OrdersTable />
-  </Fragment>
-);
+interface StateProps {
+  isNoItems: boolean;
+}
+type Props = StateProps;
+
+export const OrdersScreenInner = (props: Props): JSX.Element => {
+  const { isNoItems } = props;
+  return (
+    <NoItemsDetect isNoItems={isNoItems}>
+      <OrdersTable />
+    </NoItemsDetect>
+  );
+};
+
+// TODO: add reselect
+const mapStateToProps = (state: AppState) => {
+  const { items } = state.orders.customer;
+  return { isNoItems: items.length < 1 };
+};
+
+export const OrdersScreen = connect(mapStateToProps)(OrdersScreenInner);
